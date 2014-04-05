e5d60380b9dcf786e7211b222d337ac9c724f63c
Alan Plotko <alan.plotko@gmail.com>
2014-04-05 15:22:25 -0400

    Update template

--- amiproductive.py
+++ amiproductive.py
@@ -51,7 +51,7 @@ def index():
 
 @bottle.route('/receiveData', method="POST")
 def receiveData():
-  with open("/static/userdata/data.txt", "a") as f:
+  with open(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'static/userdata/data.txt'), "a") as f:
     f.write(bottle.request.POST)
 
 # def snippet_create(user, code):
