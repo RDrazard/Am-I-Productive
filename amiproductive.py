fadb7d3b5556bf3df3cf423c548b37b033081b5d
Alan Plotko <alan.plotko@gmail.com>
2014-04-05 11:01:53 -0400

    Update template

--- amiproductive.py
+++ amiproductive.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
+import sys, os
 import uuid
 import bottle
 import pymongo
@@ -47,7 +47,20 @@ mongo_db.authenticate(os.environ['OPENSHIFT_MONGODB_DB_USERNAME'],
 
 @bottle.route('/')
 def index():
-  return bottle.template('index')
+  return bottle.template('index', mac=getMacAddress())
+
+def getMacAddress(): 
+  if sys.platform == 'win32': 
+    for line in os.popen("ipconfig /all"): 
+      if line.lstrip().startswith('Physical Address'): 
+        mac = line.split(':')[1].strip().replace('-',':') 
+        break 
+      else: 
+        for line in os.popen("/sbin/ifconfig"): 
+          if line.find('Ether') > -1: 
+            mac = line.split()[4] 
+            break 
+  return mac  
 
 # def snippet_create(user, code):
 #   nsnippet = {
