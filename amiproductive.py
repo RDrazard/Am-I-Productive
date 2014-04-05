f58b6ee489999cfde0c0a8749eb06ebe69daeb5b
Alan Plotko <alan.plotko@gmail.com>
2014-04-05 11:13:46 -0400

    Update template

--- amiproductive.py
+++ amiproductive.py
@@ -58,7 +58,7 @@ def mac_for_ip(ip):
         try:
             if_mac = addrs[nif.AF_LINK][0]['addr']
             if_ip = addrs[nif.AF_INET][0]['addr']
-        except IndexError, KeyError: #ignore ifaces that dont have MAC or IP
+        except (IndexError, KeyError): #ignore ifaces that dont have MAC or IP
             if_mac = if_ip = None
         if if_ip == ip:
             return if_mac
