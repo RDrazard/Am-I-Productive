c6c74e46087d962d61c5e77e0092a95106ffb8a4
Alan Plotko <alan.plotko@gmail.com>
2014-04-06 03:25:24 -0400

    Update data capturing stuff.

--- amiproductive.py
+++ amiproductive.py
@@ -79,17 +79,11 @@ def index():
   except ZeroDivisionError:
     percentage = 0
 
-  with open(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'static/assets/data/flare.json'), 'w') as f:
-    d = {}
-    d['name'] = 'visited_sites'
-    d['children'] = []
+  with open(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'static/assets/data/data.tsv'), 'w') as f:
+    f.write('website\tfrequency')
     cursor = mongo_db.traffic.find({'mac' : '10:10:10:10'})
-    for record in cursor: 
-      d['children'].append({ 
-        'name': record['_id'],
-        'size': record['count']
-      })
-    f.write(json.dumps(d))
+    for record in cursor:
+      f.write(record['_id'] + '\t' + record['count'])
 
   return bottle.template('index', mac='10:10:10:10', total_requests=int(total_requests), good=int(good), bad=int(bad), percentage=percentage)
 
